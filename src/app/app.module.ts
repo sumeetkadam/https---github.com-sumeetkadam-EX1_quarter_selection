ab2722c6d036b0a2aba0f17fe5737f874fea2547
sumeetkadam <sumeetkadam22@gmail.com>
2022-07-05 19:39:18 +0530

    second commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,26 @@
-import { NgModule } from '@angular/core';
+import { NgModule } from '@angular/core'; //importing NgModule from @angular/core
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
+//if new component is created, it willbe imported first and reference will be included in declarations
+import { AppRoutingModule } from './app-routing.module'; //we enabled routing this this module is imported
 import { AppComponent } from './app.component';
 import { FooterComponent } from './footer/footer.component';
 import { HeaderComponent } from './header/header.component';
 
-@NgModule({
-  declarations: [
+
+@NgModule({ //this is decorator
+  declarations: [ //it is an array of components created.
     AppComponent,
-    HeaderComponent,
-    FooterComponent
+    HeaderComponent, 
+    FooterComponent,
+    
   ],
-  imports: [
+  imports: [ //it is an array of modules required to be used in the application.
     BrowserModule,
     AppRoutingModule,
     FormsModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
-})
+  providers: [], //this will inculde the services created.
+  bootstrap: [AppComponent]//include the main app component for starting the execution.
+})                          //this component runs first.
 export class AppModule { }
